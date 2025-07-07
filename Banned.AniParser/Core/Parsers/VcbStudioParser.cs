7704ee50a6324a8a9e3076b058ec28ab27fa5e57
banned2054 <banned2054@163.com>
2025-07-07 21:51:48 +0800

    refactor: 正则表达式精简

--- Banned.AniParser/Core/Parsers/VcbStudioParser.cs
+++ Banned.AniParser/Core/Parsers/VcbStudioParser.cs
@@ -20,7 +20,7 @@ public class VcbStudioParser : BaseParser
         {
             new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)\[(?<start>\d+)-(?<end>\d+)\]\[(?<codec>Ma10p|Ma444-10p|Hi444pp|Hi10p)?_?(?<resolution>\d+[pP])(?:_HDR)?\]\[[^\[\]]+\](?:\.(?<language>[^\[\]\.]+))?\.?(?:mp4|mkv|ass|mka)",
                 RegexOptions.IgnoreCase),
-            new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)(?:10-bit)?\s?(?<resolution>\d+[pP])\s?(?<codec>HEVC|AVC)?\s?(?:(?<source>[a-zA-Z]+[Rr]ip))\s\[(?<season>[^\[\]]+)(?:Fin)?\]",
+            new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)(?:10-bit)?\s?(?<resolution>\d+[pP])\s?(?<codec>HEVC|AVC)?\s?(?<source>[a-zA-Z]+[Rr]ip)\s\[(?<season>[^\[\]]+)(?:Fin)?\]",
                 RegexOptions.IgnoreCase),
         };
         //[流云字幕组&VCB-S&ANK-Raws] 双斩少女 / KILL la KILL / キルラキル 10-bit 1080p AVC BDRip [Reseed Fin]
