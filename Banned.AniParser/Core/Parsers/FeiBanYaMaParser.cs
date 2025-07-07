90401d1496334c71aea2ccb4768f91e47dcee07d
banned2054 <banned2054@163.com>
2025-07-07 21:51:21 +0800

    fix: 沸班亚马制作组添加爱奇艺的源

--- Banned.AniParser/Core/Parsers/FeiBanYaMaParser.cs
+++ Banned.AniParser/Core/Parsers/FeiBanYaMaParser.cs
@@ -17,10 +17,10 @@ public class FeiBanYaMaParser : BaseParser
         SingleEpisodePatterns = new List<Regex>
         {
             new(
-                @"\[Feibanyama\](?<title>[^\[\]]+?)-\s?(?<episode>\d+)(?:v(?<version>\d+))?\s?\[(?<websource>(?:CR|ABEMA|AMZN))\s(?<source>[a-zA-Z]+[Rr]ip)\s(?<resolution>\d+[pP])\s(?<codeV>HEVC-10bit)\s(?<codeA>AAC|OPUS|E-AC-3)\s(?<lang>.+?)\]",
+                @"\[Feibanyama\](?<title>[^\[\]]+?)-\s?(?<episode>\d+)(?:v(?<version>\d+))?\s?\[(?<websource>CR|ABEMA|AMZN|IQIYI)\s(?<source>[a-zA-Z]+[Rr]ip)\s(?<resolution>\d+[pP])\s(?<codeV>HEVC-10bit)\s(?<codeA>AAC|OPUS|E-AC-3)\s(?<lang>.+?)\]",
                 RegexOptions.IgnoreCase),
             new(
-                @"\[沸班亚马制作组\](?<title>[^\[\]]+?)-\s?(?<episode>\d+)(?:v(?<version>\d+))?\s?\[(?<websource>(?:CR|ABEMA|AMZN))\s(?<source>[a-zA-Z]+[Rr]ip)\s(?:AI)?(?<resolution>\d+[pP])\s(?<codeV>HEVC-10bit)\s(?<codeA>AAC|OPUS|E-AC-3)\]\[(?<lang>.+?)\]",
+                @"\[沸班亚马制作组\](?<title>[^\[\]]+?)-\s?(?<episode>\d+)(?:v(?<version>\d+))?\s?\[(?<websource>CR|ABEMA|AMZN|IQIYI)\s(?<source>[a-zA-Z]+[Rr]ip)\s(?:AI)?(?<resolution>\d+[pP])\s(?<codeV>HEVC-10bit)\s(?<codeA>AAC|OPUS|E-AC-3)\]\[(?<lang>.+?)\]",
                 RegexOptions.IgnoreCase),
         };
     }
