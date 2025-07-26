ee6365136e0e8c1d3c48437054b14914c471b65d
banned2054 <banned2054@163.com>
2025-07-26 23:49:04 +0800

    feat: 添加vcb-studio对剧场版的支持

--- Banned.AniParser/Core/Parsers/VcbStudioParser.cs
+++ Banned.AniParser/Core/Parsers/VcbStudioParser.cs
@@ -16,26 +16,38 @@ public class VcbStudioParser : BaseParser
         {
             new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)\[(?<episode>\d+(?:\.\d+)?)?(?:\(?(?<special_season>OVA|OAD)(?<special_episode>\d+)?\)?)?\]\[(?<codec>Ma10p|Ma444-10p|Hi444pp|Hi10p)?_?(?<resolution>\d+p)(?:_HDR)?\]\[[^\[\]]+\](?:\.(?<language>[^\[\]\.]+))?\.?(?:mp4|mkv|ass|mka)",
                 RegexOptions.IgnoreCase),
+            new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)(?<rate>\d+-bit)?\s?(?<resolution>\d+p)\s?(?<codec>HEVC|AVC)?\s?(?<source>[a-z]+Rip)\s\[(?<media_type>MOVIE)\s?(:?Fin|Reseed)?\]",
+                RegexOptions.IgnoreCase),
         };
         MultipleEpisodePatterns = new List<Regex>
         {
             new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)\[(?<start>\d+)-(?<end>\d+)\]\[(?<codec>Ma10p|Ma444-10p|Hi444pp|Hi10p)?_?(?<resolution>\d+p)(?:_HDR)?\]\[[^\[\]]+\](?:\.(?<language>[^\[\]\.]+))?\.?(?:mp4|mkv|ass|mka)",
                 RegexOptions.IgnoreCase),
-            new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)(?:10-bit)?\s?(?<resolution>\d+p)\s?(?<codec>HEVC|AVC)?\s?(?<source>[a-z]+Rip)\s\[(?<season>[^\[\]]+)(?:Fin)?\]",
+            new(@"\[(?<group>(?:[^\[\]]+&)?VCB-Studio(?:&[^\[\]]+)?)\](?<title>[^\[\]]+?)(?<rate>\d+-bit)?\s?(?<resolution>\d+p)\s?(?<codec>HEVC|AVC)?\s?(?<source>[a-z]+Rip)\s\[(?<season>(?!(?:movie|fin|reseed)(?:\b|[\s\]])).+?)\s?(?:Fin)?\]",
                 RegexOptions.IgnoreCase),
         };
     }
 
     protected override ParseResult CreateParsedResultSingle(Match match)
     {
-        var episode = 0f;
+        var mediaType = EnumMediaType.SingleEpisode;
+        var episode   = 1f;
         if (match.Groups["episode"].Success)
         {
             episode = float.Parse(match.Groups["episode"].Value);
         }
         else if (match.Groups["special_episode"].Success)
         {
-            episode = int.Parse(Regex.Replace(match.Groups["special_episode"].Value, @"\D+", ""));
+            episode   = int.Parse(Regex.Replace(match.Groups["special_episode"].Value, @"\D+", ""));
+            mediaType = EnumMediaType.Ova;
+        }
+
+        if (match.Groups["media_type"].Success)
+        {
+            if (match.Groups["media_type"].Value.ToLower() == "movie")
+            {
+                mediaType = EnumMediaType.Movie;
+            }
         }
 
         var (lang, subType) = DetectLanguageSubtitle(match.Groups["lang"].Value);
@@ -48,7 +60,7 @@ public class VcbStudioParser : BaseParser
 
         return new ParseResult
         {
-            MediaType    = EnumMediaType.SingleEpisode,
+            MediaType    = mediaType,
             Title        = title,
             Episode      = episode,
             Group        = GroupName,
@@ -62,6 +74,8 @@ public class VcbStudioParser : BaseParser
 
     protected override ParseResult CreateParsedResultMultiple(Match match)
     {
+        var mediaType    = EnumMediaType.MultipleEpisode;
+        var title        = match.Groups["title"].Value.Trim();
         var startEpisode = 0;
         var endEpisode   = 0;
         if (match.Groups["start"].Success)
@@ -74,10 +88,14 @@ public class VcbStudioParser : BaseParser
             endEpisode = int.Parse(Regex.Replace(match.Groups["end"].Value.Trim(), @"\D+", ""));
         }
 
+        if (title.Contains("剧场版"))
+        {
+            mediaType = EnumMediaType.Movie;
+        }
 
         return new ParseResult
         {
-            MediaType    = EnumMediaType.MultipleEpisode,
+            MediaType    = mediaType,
             Title        = match.Groups["title"].Value.Trim(),
             StartEpisode = startEpisode,
             EndEpisode   = endEpisode,
