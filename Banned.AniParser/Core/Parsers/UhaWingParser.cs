14e330872c8271f08cb0e07b29529f2aad15559a
banned2054 <banned2054@163.com>
2025-07-27 11:29:33 +0800

    feat: 添加对简日繁日的支持，同时添加对torrent里文件的匹配

--- Banned.AniParser/Core/Parsers/UhaWingParser.cs
+++ Banned.AniParser/Core/Parsers/UhaWingParser.cs
@@ -12,23 +12,28 @@ public class UhaWingParser : BaseParser
 
     public UhaWingParser()
     {
-        SubtitleTypeMap["CHT"] = EnumSubtitleType.Embedded;
-        SubtitleTypeMap["CHS"] = EnumSubtitleType.Embedded;
+        LanguageMap["CHS_JP"] = EnumLanguage.JpSc;
+        LanguageMap["CHT_JP"] = EnumLanguage.JpTc;
+
+        SubtitleTypeMap["CHS"]    = EnumSubtitleType.Embedded;
+        SubtitleTypeMap["CHT"]    = EnumSubtitleType.Embedded;
+        SubtitleTypeMap["CHS_JP"] = EnumSubtitleType.Embedded;
+        SubtitleTypeMap["CHT_JP"] = EnumSubtitleType.Embedded;
+
         SingleEpisodePatterns = new List<Regex>
         {
             new(
-                @"【悠哈璃羽字幕[社组]】\s?\[(?<title>[^\[\]]+?)\]\[(?<episode>\d+)(?:v(?<version>\d+))?\]\[(?:x264\s)?(?<resolution>\d+p)\]\[(?<lang>.+?)\]",
+                @"(?:【悠哈璃羽字幕[社组]】|\[UHA-Wing\])\s?\[(?<title>[^\[\]]+?)\]\[(?<episode>\d+)(?:v(?<version>\d+))?\]\[(?:x264\s)?(?<resolution>\d+p)\]\[(?<lang>.+?)\]",
                 RegexOptions.IgnoreCase),
             new(
-                @"【悠哈璃羽字幕[社组]】\s?\[(?<title>[^\[\]]+?)\]\[(?<media_type>Movie)\]\[(?<source>[a-z]+Rip)\s(?<resolution>\d+x\d+)\s(?<codeV>HEVC-?[a-z0-9]+|x264|x265)\s(?<codeA>FLAC|AAC)\]\[(?<extension>MKV|MP4)?\s?(?<lang>.+?)\]",
+                @"(?:【悠哈璃羽字幕[社组]】|\[UHA-Wing\])\s?\[(?<title>[^\[\]]+?)\]\[(?<media_type>Movie)\]\[(?<source>[a-z]+Rip)\s(?<resolution>\d+x\d+)\s(?<codeV>HEVC-?[a-z0-9]+|x264|x265)\s(?<codeA>FLAC|AAC)\]\[(?<extension>MKV|MP4)?\s?(?<lang>.+?)\]",
                 RegexOptions.IgnoreCase),
             new(
-                @"【悠哈璃羽字幕[社组]】\s?\[(?<title>[^\[\]]+?)\]\[(?<media_type>Movie)\]\[(?<codeV>x264|x265)\s(?<resolution>\d+p)\]\[(?<lang>.+?)\]",
+                @"(?:【悠哈璃羽字幕[社组]】|\[UHA-Wing\])\s?\[(?<title>[^\[\]]+?)\](?:\[(?<version>v\d+)\])?\[(?<media_type>Movie)\]\[(?<codeV>x264|x265)\s(?<resolution>\d+p)\]\[(?<lang>.+?)\]",
                 RegexOptions.IgnoreCase),
             new(
-                @"【悠哈璃羽字幕[社组]】\s?\[(?<title>[^\[\]]+?)\]\[(?<lang>.+?)\]\[(?<media_type>Movie)\]\[(?<source>[a-z]+Rip)\s(?<resolution>\d+x\d+)\s?(?<codeV>HEVC-?[a-z0-9]+|x264|x265)?\s?(?<codeA>FLAC|AAC)?\]",
+                @"(?:【悠哈璃羽字幕[社组]】|\[UHA-Wing\])\s?\[(?<title>[^\[\]]+?)\]\[(?<lang>.+?)\]\[(?<media_type>Movie)\]\[(?<source>[a-z]+Rip)\s(?<resolution>\d+x\d+)\s?(?<codeV>HEVC-?[a-z0-9]+|x264|x265)?\s?(?<codeA>FLAC|AAC)?\]",
                 RegexOptions.IgnoreCase),
-            //【悠哈璃羽字幕社】 [摇曳露营剧场版_Yuru Camp Movie][简日双语][MOVIE][WEBRIP 1920x1080]
             new(
                 @"\[UHA-Wing\]\[(?<title>[^\[\]]+?)\]\[(?<episode>\d+)(?:v(?<version>\d+))?\]\[(?<resolution>\d+p)\]\[(?<lang>.+?)\]",
                 RegexOptions.IgnoreCase),
