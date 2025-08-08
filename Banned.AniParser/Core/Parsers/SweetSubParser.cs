77e5b847837f81ec3bc56829f1bbf755c38a2db2
banned2054 <banned2054@163.com>
2025-08-08 18:44:14 +0800

    feat: 精简表达式，同时精确化

--- Banned.AniParser/Core/Parsers/SweetSubParser.cs
+++ Banned.AniParser/Core/Parsers/SweetSubParser.cs
@@ -13,16 +13,16 @@ public class SweetSubParser : BaseParser
         SingleEpisodePatterns =
         [
             new(
-                @"\[SweetSub\]\[(?<title>[^\[\]]+?)\]\[(?<engTitle>[^\[\]]+?)\]\[(?<episode>\d+)(?:v(?<version>\d+))?\]\[(?<source>[a-z]+Rip)\]\[(?<resolution>\d+p)\]\[[^\[\]]*\]\[(?<lang>.+?)\]",
+                @"\[SweetSub]\[(?<title>[\u4e00-\u9fa5a-z0-9\s\p{P}]+)]\[(?<engTitle>[a-z0-9\s\p{P}]+)]\[(?<episode>\d+)(?:v(?<version>\d+))?]\[(?<source>[a-z]+Rip)]\[(?<resolution>\d+p)]\[(?<codeV>AVC|AV1)\s(?<rate>\d+bit)]\[(?<lang>.+?)]",
                 RegexOptions.IgnoreCase),
             new(
-                @"\[SweetSub\](?<title>[^\[\]]+?)-\s(?<episode>\d+)(?:v(?<version>\d+))?\s?\[(?<source>[a-z]+Rip)\]\[(?<resolution>\d+p)\]\[[^\[\]]*\]\[(?<lang>.+?)\]",
+                @"\[SweetSub](?<title>[\u4e00-\u9fa5a-z0-9\s\p{P}]+)-\s(?<episode>\d+)(?:v(?<version>\d+))?\s?\[(?<source>[a-z]+Rip)]\[(?<resolution>\d+p)]\[(?<codeV>AVC|AV1)\s(?<rate>\d+bit)]\[(?<lang>.+?)]",
                 RegexOptions.IgnoreCase),
         ];
         MultipleEpisodePatterns =
         [
             new(
-                @"\[SweetSub\]\[(?<title>[^\[\]]+?)\]\[(?<engTitle>[^\[\]]+?)\]\[(?<start>\d+)-(?<end>\d+)\s?(?<OAD>[a-z\u4e00-\u9fff]+)?\]\[(?<source>[a-z]+Rip)\]\[(?<resolution>\d+p)\]\[[^\[\]]*\]\[(?<lang>.+?)\](\[(?:v(?<version1>\d+))\])?",
+                @"\[SweetSub]\[(?<title>[\u4e00-\u9fa5a-z0-9\s\p{P}]+)]\[(?<engTitle>[a-z0-9\s\p{P}]+)]\[(?<start>\d+)-(?<end>\d+)\s?(?<OAD>[a-z\u4e00-\u9fff]+)?]\[(?<source>[a-z]+Rip)]\[(?<resolution>\d+p)]\[(?<codeV>AVC|AV1)\s(?<rate>\d+bit)]\[(?<lang>.+?)](\[(?:v(?<version1>\d+))])?",
                 RegexOptions.IgnoreCase),
         ];
     }
