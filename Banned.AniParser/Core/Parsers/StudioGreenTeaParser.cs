b5e7c75bcb46fa44b1d583eb0a61ae5391f8034d
banned2054 <banned2054@163.com>
2025-08-08 18:43:45 +0800

    feat: 添加绿茶字幕组

---
+++ Banned.AniParser/Core/Parsers/StudioGreenTeaParser.cs
@@ -0,0 +1,23 @@
+using System.Text.RegularExpressions;
+using Banned.AniParser.Models.Enums;
+
+namespace Banned.AniParser.Core.Parsers;
+
+public class StudioGreenTeaParser : BaseParser
+{
+    public override string        GroupName => "绿茶字幕组";
+    public override EnumGroupType GroupType => EnumGroupType.Translation;
+
+    public StudioGreenTeaParser()
+    {
+        LanguageMap["ASSx2"]     = EnumLanguage.JpScTc;
+        SubtitleTypeMap["ASSx2"] = EnumSubtitleType.Muxed;
+        SingleEpisodePatterns =
+        [
+            new(@"\[绿茶字幕组](?<title>[^\[\]]+?)\[(?<episode>\d+)(?:v(?<version>\d+))?](?:\[(?<source>WebRip)])?\[(?<codeV>HEVC)_?(?<codeA>AAC)]\[(?<resolution>\d+p)]\[(?<lang>.+?)]",
+                RegexOptions.IgnoreCase),
+            new(@"\[Studio\sGreenTea](?<title>[^\[\]]+?)\[(?<episode>\d+)(?:v(?<version>\d+))?]\[(?<source>WebRip)]\[(?<codeV>HEVC)-?(?<rate>\d+bit)?\s?(?<resolution>\d+p)\s?(?<codeA>AAC)(:?]\[|\s)(?<lang>.+?)]",
+                RegexOptions.IgnoreCase),
+        ];
+    }
+}
